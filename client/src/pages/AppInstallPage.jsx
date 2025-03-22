b7f6bc32924b3e1a0cf9741833958d8d5f3cd84e
HanaKs <ngsw1816yk@gmail.com>
2025-03-22 17:37:08 +0900

    Windowsの場合を追加

--- client/src/pages/AppInstallPage.jsx
+++ client/src/pages/AppInstallPage.jsx
@@ -9,7 +9,26 @@ const AppInstallPage = () => {
             </Typography>
             <Typography variant="body1" paragraph>
                 このアプリをより便利に使うために、スマートフォンのホーム画面に追加してください。
+                ホーム画面に追加すると、ブラウザを開かずにアプリをすぐに起動できます。
             </Typography>
+
+            <Box sx={{ marginBottom: 4 }}>
+                <Typography variant="h6" gutterBottom>
+                    Windows の場合:
+                </Typography>
+                <Typography variant="body2" paragraph>
+                    1. 画面右上のメニュー（3点アイコン）をタップします。<br />
+                    2. 「キャスト、保存、共有 {'>'} ページをアプリとしてインストール」を選択します。<br />
+                    3. 名前を入力して「追加」をタップします。
+                </Typography>
+                {/* Android スクリーンショットをここに表示 */}
+                <Box
+                    component="img"
+                    src="/images/android-add-to-home.png" // スクリーンショットのパス
+                    alt="Android のホーム画面に追加"
+                    sx={{ width: '100%', maxWidth: 300, borderRadius: 2, boxShadow: 3 }}
+                />
+            </Box>
 
             <Box sx={{ marginBottom: 4 }}>
                 <Typography variant="h6" gutterBottom>
@@ -46,10 +65,6 @@ const AppInstallPage = () => {
                     sx={{ width: '100%', maxWidth: 300, borderRadius: 2, boxShadow: 3 }}
                 />
             </Box>
-
-            <Typography variant="body1" paragraph>
-                ホーム画面に追加すると、ブラウザを開かずにアプリをすぐに起動できます。
-            </Typography>
         </Container>
     );
 };
